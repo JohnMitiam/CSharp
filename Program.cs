3efe21cbada7a8a011e359062f6a8df0b4cd3eda
JOHNMITIAM <johnjohnmitiam14@gmail.com>
2024-10-09 19:45:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+
+string firstName = "   John   ";
+
+firstName = firstName.Trim();
+
+string lastName = "   Mitiam   ";
+
+Console.WriteLine();
+
+string name = $"I am {firstName} {lastName.Trim()}";
+
+Console.WriteLine(name.ToUpper());
+
+Console.WriteLine(name);
+
+Console.WriteLine(name.StartsWith("I"));
+
+Console.WriteLine(name.Length);
