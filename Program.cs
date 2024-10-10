2d00972ca0007631f375d0d6391bc23e388eb061
JOHNMITIAM <johnjohnmitiam14@gmail.com>
2024-10-10 21:56:30 +0800

    LINQ

--- Program.cs
+++ Program.cs
@@ -1,39 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
-Console.WriteLine("Its me Jacko!");
-Console.WriteLine(" ");
+﻿
+List<int> scores = [3, 45, 82, 97, 92, 100, 81, 60];
 
-Console.WriteLine("*We are Learning Integers");
-decimal a = 42.1M; // explicit type
-Console.WriteLine($"Integer a = {a}");
-decimal b = 38.2M;
-Console.WriteLine($"Integer b = {b}");
-decimal c = a + b;
-Console.WriteLine($"The answer to a + b is {c}");
-Console.WriteLine(" ");
+// LINQ
+//IEnumerable<int> scoreQuery =
+//    from score in scores
+//    where score > 80
+//    orderby score descending
+//    //select $"The score is lower 80 is {score}";
+//    select score;
 
-Console.WriteLine("*We're Learning IF's Statements");
-int d = 5;
-int e = 5;
-int f = 10;
-int g = d + e + f;
+// LINQ
+var scoreQuery = scores.Where(s => s > 80)
+                       .OrderByDescending(s => s);
 
+List<int> myScores = scoreQuery.ToList();
 
-if (g == 20)
-{
-    Console.WriteLine("The answer is equal to 20");
-}
-else
-{
-    if ((g < 20) && (d == e))
-    {
-        Console.WriteLine("The first numer is equal to the second");
-        Console.WriteLine($"The Answer is less than 20 so, integer D + integer E is {f}");
-
-    }
-    else
-    {
-        Console.WriteLine("the answer is greater than 20");
-        Console.WriteLine("Or the first number is not equal to the second");
-    }
+foreach (var score in myScores)
+{ 
+    Console.WriteLine(score);
 }
